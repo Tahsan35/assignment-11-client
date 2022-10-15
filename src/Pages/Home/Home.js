ae8ec92729ab2812c926824327fdd827a63f797c
Tahsan <tahsanknmas14@gmail.com>
2022-10-15 10:14:25 +0600

    added testimonial component

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Slider from './Slider';
+import Testimonial from './Testimonial';
 
 const Home = () => {
     return (
         <div>
             <Slider></Slider>
+            <Testimonial></Testimonial>
         </div>
     );
 };
