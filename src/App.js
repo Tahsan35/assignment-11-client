ef8646bb4595a30d9fd0ef331362afbd375d9ad2
Tahsan <tahsanknmas14@gmail.com>
2022-10-15 08:48:28 +0600

    added blogs page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+
+import { Route, Routes } from "react-router-dom";
+import Blogs from "./Pages/Blogs/Blogs";
 import Navbar from "./Pages/Navbar/Navbar";
 import Footer from "./Pages/Shared/Footer";
 
@@ -5,6 +8,9 @@ function App() {
   return (
     <div>
       <Navbar></Navbar>
+      <Routes>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+      </Routes>
       <Footer></Footer>
     </div>
   );
