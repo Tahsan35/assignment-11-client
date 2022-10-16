4d5d2e624b3f550a0363711a7e99159a4ebcb71c
Tahsan <tahsanknmas14@gmail.com>
2022-10-16 22:13:09 +0600

    added products items

---
+++ src/Pages/ProductsDetails/MyItems.js
@@ -0,0 +1,50 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import MyItemsDetails from '../ProductsDetails/myItemsDetails';
+
+const MyItems = () => {
+
+    const [user] = useAuthState(auth);
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const getUploadITem = async () => {
+            const email = user?.email;
+            const url = `https://ancient-bayou-60727.herokuapp.com/upload?email=${email}`;
+            const { data } = await axios.get(url);
+            setProducts(data);
+        }
+        getUploadITem();
+    }, [products]);
+
+
+    const handleDeleteItem = (id) => {
+        const proceed = window.confirm('Are you Sure');
+        if (proceed) {
+            const url = `https://ancient-bayou-60727.herokuapp.com/upload/${id}`
+            fetch(url, {
+                method: 'DELETE',
+            })
+                .then(res => res.json())
+                .then(data => {
+                    const remaining = products.filter(item => item._id !== id);
+                    setProducts(remaining);
+                })
+        }
+    }
+    return (
+        <div className='my-16'>
+            {
+                products.map(details => <MyItemsDetails
+                    key={details._id}
+                    details={details}
+                    handleDeleteItem={handleDeleteItem}
+                ></MyItemsDetails>)
+            }
+        </div>
+    );
+};
+
+export default MyItems;
